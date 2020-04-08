7f869d24dd4533fec4bd7548d632f34153935827
Оксана <oks-forst89@yandex.ru>
2020-04-08 23:27:21 +0700

    4

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -2,16 +2,14 @@ package com.company;
 import java.util.Scanner;
 import java.io.*;
 
-
-
 public class Main {
-
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Введите размер таблицы: ");//просим ввести число для размера таблицы
         int num = in.nextInt();
         in.close();
 
+        if (num>0) {
         int size=num*num; // вычисляем максимальное число в таблице
         //System.out.println(size); // выводим на печать максимальное число в таблице
         String size1 = String.valueOf(size); // переводим число в строку
@@ -22,7 +20,6 @@ public class Main {
         String str= "-";
         String str1= "+";
         String text=" ";
-        int Value=0;
         String separator = (str.repeat(lenght)+str1);
         String separator1 =separator.repeat(num-1)+str.repeat(lenght); //строка с +-
 
@@ -32,14 +29,17 @@ public class Main {
                 int value = (i+1)*(j+1);
                 String tf = "%"+lenght+"d"+"|"; // задаем параметр форматирования для printf
                 String tf1 = "%"+lenght+ 'd'+ text;
-
+                if (j<num-1){
                 System.out.printf(tf, value); // печатаем отформатированную матрицу
             }
-            System.out.println();
-            System.out.println(separator1); // вывод строки с +-
-
-
+                else System.out.printf(tf1, value); // печатаем отформатированную матрицу
             }
+            System.out.println();
+            if (i<num-1){
+            System.out.println(separator1); // вывод строки с +-
+            }}
         }
+    else System.out.println("Введите число больше 0");
+    }
     }
 
