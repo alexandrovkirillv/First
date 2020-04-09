1981cc7f00ff411f01c1d000fe3dc22003d23c51
Оксана <oks-forst89@yandex.ru>
2020-04-09 22:19:52 +0700

    4

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,4 +1,5 @@
 package com.company;
+
 import java.util.Scanner;
 import java.io.*;
 
@@ -9,37 +10,32 @@ public class Main {
         int num = in.nextInt();
         in.close();
 
-        if (num>0) {
-        int size=num*num; // вычисляем максимальное число в таблице
-        //System.out.println(size); // выводим на печать максимальное число в таблице
-        String size1 = String.valueOf(size); // переводим число в строку
+        if (num > 0) {
+            int size = 32 * 32;
+            String size1 = String.valueOf(size);
 
-        int lenght = size1.length(); //считаем количество разрядов в числе
-        //System.out.println(lenght); // выводим на печать количество разрядов в числе
+            int lenght = size1.length();
 
-        String str= "-";
-        String str1= "+";
-        String text=" ";
-        String separator = (str.repeat(lenght)+str1);
-        String separator1 =separator.repeat(num-1)+str.repeat(lenght); //строка с +-
+            String str = "-";
+            String str1 = "+";
+            String separator = (str.repeat(lenght) + str1);
+            String separator1 = separator.repeat(num - 1) + str.repeat(lenght);
 
-        for (int i = 0; i < num; i++) {
-            for (int j = 0; j < num; j++)
-            {
-                int value = (i+1)*(j+1);
-                String tf = "%"+lenght+"d"+"|"; // задаем параметр форматирования для printf
-                String tf1 = "%"+lenght+ 'd'+ text;
-                if (j<num-1){
-                System.out.printf(tf, value); // печатаем отформатированную матрицу
+            for (int i = 0; i < num; i++) {
+                for (int j = 0; j < num; j++) {
+                    int value = (i + 1) * (j + 1);
+                    String tf = "%" + lenght + "d" + "|";
+                    String tf1 = "%" + lenght + 'd' + " ";
+                    if (j < num - 1) {
+                        System.out.printf(tf, value);
+                    } else System.out.printf(tf1, value);
+                }
+                System.out.println();
+                if (i < num - 1) {
+                    System.out.println(separator1);
+                }
             }
-                else System.out.printf(tf1, value); // печатаем отформатированную матрицу
-            }
-            System.out.println();
-            if (i<num-1){
-            System.out.println(separator1); // вывод строки с +-
-            }}
-        }
-    else System.out.println("Введите число больше 0");
+        } else System.out.println("Введите число больше 0");
     }
-    }
+}
 
